da31b90af3731f08308661ce54047cf03a1f2bc5
Alexander Kim <akim@urthecast.com>
2018-08-30 21:45:56 -0400

    add demo

--- mi_reg.py
+++ mi_reg.py
@@ -5,33 +5,33 @@ from scipy.optimize import differential_evolution
 FILTER_SIGMA = 1.5
 BOUNDS = [(-10, 10), (-10, 10)]  # Bounds (in pixels) supported by mutual information based correlator
 
-def main_mi_reg(ref_chip, cmp_chip, bounds=BOUNDS):
+def main_mi_reg(ref_image, cmp_image, bounds=BOUNDS):
     """
     Correlator based onMutual Information Algorithm
     http://www.sci.utah.edu/~fletcher/CS7960/slides/Yen-Yun.pdf
-    :param ref_chip: ndarray, containing reference chip data
-    :param cmp_chip: ndarray, containing comparison chip data
+    :param ref_image: ndarray, containing reference image data
+    :param cmp_image: ndarray, containing comparison image data
     :param bounds: sequence, bounds paramater in scipy.optimize.differential_evolution
     :return: (residual in X, residual in Y, match height)
     """
-    obj_func = lambda dx_dy: -__mutual_information(shift(ref_chip, dx_dy), cmp_chip)
+    obj_func = lambda dx_dy: -__mutual_information(shift(ref_image, dx_dy), cmp_image)
     opt_res = differential_evolution(obj_func, bounds)
     (dx, dy), match_height = -opt_res.x, -opt_res.fun
     return dx, dy, match_height
 
-def __mutual_information(ref_chip_crop, cmp_chip, bins=256, normed=True):
+def __mutual_information(ref_image_crop, cmp_image, bins=256, normed=True):
     """
-    :param ref_chip_crop: ndarray, cropped image from the center of reference chip, needs to be same size as `cmp_chip`
-    :param cmp_chip: ndarray, comparison chip data data
+    :param ref_image_crop: ndarray, cropped image from the center of reference image, needs to be same size as `cmp_image`
+    :param cmp_image: ndarray, comparison image data data
     :param bins: number of histogram bins
     :param normed: return normalized mutual information
     :return: mutual information values
     """
-    ref_range = (ref_chip_crop.min(), ref_chip_crop.max())
-    cmp_range = (cmp_chip.min(), cmp_chip.max())
-    joint_hist, _, _ = np.histogram2d(ref_chip_crop.flatten(), cmp_chip.flatten(), bins=bins, range=[ref_range, cmp_range])
-    ref_hist, _ = np.histogram(ref_chip_crop, bins=bins, range=ref_range)
-    cmp_hist, _ = np.histogram(cmp_chip, bins=bins, range=cmp_range)
+    ref_range = (ref_image_crop.min(), ref_image_crop.max())
+    cmp_range = (cmp_image.min(), cmp_image.max())
+    joint_hist, _, _ = np.histogram2d(ref_image_crop.flatten(), cmp_image.flatten(), bins=bins, range=[ref_range, cmp_range])
+    ref_hist, _ = np.histogram(ref_image_crop, bins=bins, range=ref_range)
+    cmp_hist, _ = np.histogram(cmp_image, bins=bins, range=cmp_range)
     joint_ent = __entropy(joint_hist)
     ref_ent = __entropy(ref_hist)
     cmp_ent = __entropy(cmp_hist)
--- poc_reg.py
+++ poc_reg.py
@@ -31,41 +31,41 @@ def poc_model(al, dt1, dt2, poc, fitting_area):
                           / (sin((n1 + dt1) * pi / N1) * sin((n2 + dt2) * pi / N2) * (N1 * N2))
 
 
-def pocfunc(ref_chip, cmp_chip):
+def pocfunc(ref_image, cmp_image):
     # Windowing to reduce boundary effects
-    hanning_window_x = np.hanning(ref_chip.shape[0])
-    hanning_window_y = np.hanning(ref_chip.shape[1])
+    hanning_window_x = np.hanning(ref_image.shape[0])
+    hanning_window_y = np.hanning(ref_image.shape[1])
     hanning_window_2d = hanning_window_x.reshape(hanning_window_x.shape[0], 1) * hanning_window_y
-    ref_chip, cmp_chip = [ref_chip, cmp_chip] * hanning_window_2d
-    F = scipy.fftpack.fft2(ref_chip)
-    G = scipy.fftpack.fft2(cmp_chip)
+    ref_image, cmp_image = [ref_image, cmp_image] * hanning_window_2d
+    F = scipy.fftpack.fft2(ref_image)
+    G = scipy.fftpack.fft2(cmp_image)
     G_ = np.conj(G)
     R = F * G_ / np.abs(F * G_)  # cross-phase spectrum
     R = scipy.fftpack.fftshift(R)
 
     # Spectral weighting technique to reduce aliasing and noise effects
-    M = np.floor([ref_chip.shape[0] / 2.0, ref_chip.shape[1] / 2.0])  # M = [M1, M2]
+    M = np.floor([ref_image.shape[0] / 2.0, ref_image.shape[1] / 2.0])  # M = [M1, M2]
     U = M / 2.0  # U = [U1, U2]
     low_pass_filter = np.ones([int(M[0]) + 1, int(M[1]) + 1])
-    low_pass_filter = zero_padding(low_pass_filter, ref_chip.shape, U)
+    low_pass_filter = zero_padding(low_pass_filter, ref_image.shape, U)
     R = R * low_pass_filter
     R = scipy.fftpack.fftshift(R)
     poc = scipy.fftpack.fftshift(np.real(scipy.fftpack.ifft2(R)))
     return poc
 
 
-def main_poc_reg(ref_chip, cmp_chip):
+def main_poc_reg(ref_image, cmp_image):
     # calculate phase-only correlation
-    poc = pocfunc(ref_chip, cmp_chip)
+    poc = pocfunc(ref_image, cmp_image)
     # get peak position peak
     max_pos = np.argmax(poc)
-    peak = np.array([max_pos / ref_chip.shape[1], max_pos % ref_chip.shape[1]]).astype(int)
+    peak = np.array([max_pos / ref_image.shape[1], max_pos % ref_image.shape[1]]).astype(int)
     # fitting using least-square method
     mc = np.array([FITTING_SHAPE[0] / 2.0, FITTING_SHAPE[1] / 2.0])
     fitting_area = poc[int(peak[0] - mc[0]): int(peak[0] + mc[0] + 1), int(peak[1] - mc[1]): int(peak[1] + mc[1] + 1)]
     if fitting_area.shape != (FITTING_SHAPE[0] + 1, FITTING_SHAPE[1] + 1):
         return 0.0, 0.0, 0.0
-    m = np.array([ref_chip.shape[0] / 2.0, ref_chip.shape[1] / 2.0])
+    m = np.array([ref_image.shape[0] / 2.0, ref_image.shape[1] / 2.0])
     u = (m / 2)
     y, x = np.mgrid[-mc[0]:mc[0] + 1, -mc[1]:mc[1] + 1]
     y = np.ceil(y + peak[0] - m[0])
